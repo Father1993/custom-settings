7d7559f2e445504eeb4a07d2e984e667ee9a6d65
Father1993 <enjoyhillol@gmail.com>
2025-06-04 19:00:59 +1000

    update: add new fn and aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,4 +1,3 @@
-alias cls=clear
 alias n=nvim
 alias open=explorer.exe
 alias cp='cp -i'
@@ -6,15 +5,42 @@ alias mv='mv -i'
 alias myip='curl ifconfig.me'
 alias pingg='ping google.com'
 alias rm='rm -i'
+alias cdh=cd /home/father
+
+# Git
+alias gdd='git add .'
+alias gpushd='git push origin dev'
+alias grep='grep --color=auto'
+alias gst='git status'
+alias gpull='git checkout master && git fetch && git pull && git checkout dev && git merge master && git push origin dev'
+alias aicom='python CommitPilot/auto_commit.py -c'
+alias aipush='python CommitPilot/auto_commit.py'
+
+# Servers
 alias smk='ssh root@45.153.68.101'
 alias smkbit='ssh bitrix@45.153.68.101'
-alias cdh=cd /home/father
-alias gdd='git add .'
+
+# Others
+alias xzegrep='xzegrep --color=auto'
+alias xzfgrep='xzfgrep --color=auto'
+alias xzgrep='xzgrep --color=auto'
+alias zegrep='zegrep --color=auto'
+alias zfgrep='zfgrep --color=auto'
+alias zgrep='zgrep --color=auto'
+
 #alias pbcopy="win32yank.exe -i"
+
+# Функия для коммита без ковычек использя alias
+gcom() {
+    if [[ $# -eq 0 ]]; then
+        echo "Ошибка: Нет сообщения для коммита"
+        return 1
+    fi
+    git commit -m "$*"
+}
 
 # Password generator
 pwgen() {
   local len=${1:-20}
   LC_ALL=C tr -dc 'A-Za-z0-9@#%^&*()_+=-{}[]:;<>,.?/' < /dev/urandom | head -c "$len" | xargs echo
 }
-
